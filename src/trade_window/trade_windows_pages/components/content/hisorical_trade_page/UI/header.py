c4648d394420b6e6e760d643c7b8ffecd492d031
MIN <maksimivankov26@yandex.ru>
2024-07-31 20:34:14 +0300

    РАБОТА

---
+++ src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/UI/header.py
@@ -0,0 +1,61 @@
+
+import flet as ft
+from variable import *
+from imports import *
+
+class Header(ft.UserControl):
+    # def __init__(self,change_menu):
+    #     super().__init__()
+    #     self.change_menu = change_menu
+
+
+    def build(self):
+        
+        self.header = ft.Container(
+            # ft.Column(controls=[
+            #     ft.Container(
+            #         ft.Row(controls=[
+            #           ft.Container(ft.ElevatedButton(content = ft.Text('Информация',size=12,),bgcolor=c_yelow,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,margin=7,height=30),
+            #           ft.Container(ft.ElevatedButton(content = ft.Text('История торговли',size=12,),bgcolor=c_yelow,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,margin=7,height=30),
+            #     ]),bgcolor='red',width=350,alignment=ft.alignment.center,
+            #     ),
+            # ]),
+            # expand=2,
+            ft.Column(
+                controls=[
+                    ft.Container(
+                        ft.Container(
+                            ft.Container(
+                                ft.Row(controls=[
+                                    ft.Container(ft.ElevatedButton(content = ft.Text('Информация',size=12,),bgcolor=c_yelow,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,margin=7,height=30),
+                                    ft.Container(ft.ElevatedButton(content = ft.Text('История торговли',size=12,),bgcolor=c_yelow,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,margin=7,height=30),
+                                ]),width=320),alignment=ft.alignment.center),height=60,margin=ft.margin.only(top=-14,left=-10,right=-10,bottom=-14),      
+                    ),
+                    ft.Container(
+                        ft.Container(alignment=ft.alignment.center),height=1,bgcolor=c_white,margin=ft.margin.only(top=2,left=-10,right=-10),      
+                    ),
+                    ft.Container(
+                        ft.Container(
+                            ft.Column(controls=[
+                                ft.Text('Свободный фрейм - торговля на 1 стратегии по выбранному небольшому фрейму данных \nТорговля со смещением - торговля на заданном небольшом фрейме данных интервалами со смещением \nИсторическая торговля  - торговля на длинном фрейме данных',
+                                        size=12,color=c_white,text_align='center'),
+                                ft.Container(#11
+                                    ft.Row(controls=[
+                                        ft.Container(ft.ElevatedButton(content = ft.Text('\nСвободный\nфрейм',size=12,text_align='center',height=70),bgcolor=c_yelow,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,margin=7),
+                                        ft.Container(ft.ElevatedButton(content = ft.Text('\nТорговля\nсо смещением',size=12,text_align='center',height=70),bgcolor=c_yelow,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,margin=7),
+                                        ft.Container(ft.ElevatedButton(content = ft.Text('\nИсторическая\nторговля',size=12,text_align='center',height=70),bgcolor=c_yelow,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,margin=7),
+                                    ]),
+                                    width=440,
+                                    height=80,
+                                    margin=ft.margin.only(left=90)
+                                ),
+                            ]),
+                            alignment=ft.alignment.center),margin=ft.margin.only(top=2,left=-10,right=-10),padding=ft.padding.only(top=10)      
+                    ),
+                    
+                ]
+            ),expand=2
+            
+        )
+        
+        return self.header
--- src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/hisorical_trade_page.py
+++ src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/hisorical_trade_page.py
@@ -3,15 +3,28 @@ import flet as ft
 from variable import *
 from imports import *
 
+from src.trade_window.trade_windows_pages.components.content.hisorical_trade_page.UI.header import Header
+
 class Hisorical_trade_page(ft.UserControl):
     # def __init__(self,change_menu):
     #     super().__init__()
     #     self.change_menu = change_menu
 
     def build(self):
-        
         self.hisorical_trade_page = ft.Container(
-            ft.Text('Историческая торговля')
+            ft.Column(
+                controls=[
+                    ft.Container(
+                        ft.Container(ft.Text('Торговля по историческим данным',color=c_blue),alignment=ft.alignment.center),bgcolor=c_yelow,height=28,margin=ft.margin.only(top=-10,left=-10,right=-10),border = ft.border.all(1,c_white), 
+                    ),
+                    ft.Container(
+                        ft.Column(controls=[
+                            Header(),
+                        ]),
+                        expand=2,
+                    )
+                ]
+            ),expand=2
         )
         
         return self.hisorical_trade_page
