5354ee3d588a142c99cfcada360c8f04c26ed5d4
MIN <maksimivankov26@yandex.ru>
2024-08-04 07:41:32 +0300

    работа

--- src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/pages/change_strategy_trade_page/change_strategy_trade_page.py
+++ src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/pages/change_strategy_trade_page/change_strategy_trade_page.py
@@ -14,25 +14,10 @@ class Change_strategy_trade_page(ft.UserControl):
     def __init__(self,change_page):
         super().__init__()
         self.change_page = change_page
-
-    def input_name_bot(self,e):
-        print(e.control.value)
-    def input_komission_mayker(self,e):
-        print(e.control.value)
-    def input_komission_taker(self,e):
-        print(e.control.value)
-    def input_deposit(self,e):
-        print(e.control.value)
-    def input_leverage(self,e):
-        print(e.control.value)
-    def input_tp(self,e):
-        print(e.control.value)
-    def input_sl(self,e):
-        print(e.control.value)
-    def input_volume_min(self,e):
-        print(e.control.value)
-    def input_volume_max(self,e):
-        print(e.control.value)
+        self.strategy_translate = {
+            'one':'Канал, тренд, локаль, объём',
+            'MA':'Скользящие средние'
+        }
 
     def build(self):
         self.change_strategy_trade_page = ft.Container(
@@ -40,7 +25,7 @@ class Change_strategy_trade_page(ft.UserControl):
                         ft.Container(
                             ft.Column(controls=[
                                 ft.Container(ft.Text('Выберите одну или несколько стратегий реальной тестовой торговли',
-                                        size=12,color=c_white,text_align='center',),padding=ft.padding.only(left=120)),
+                                        size=12,color=c_white,text_align='center',),padding=ft.padding.only(left=230)),
                                 ft.Container(
                                     ft.Container(
                                         ft.Column(controls=[
@@ -50,28 +35,24 @@ class Change_strategy_trade_page(ft.UserControl):
                                                         ft.Container(ft.Text('Выбор стратегии',color=c_blue,),bgcolor=c_yelow,padding=5,margin=ft.margin.only(bottom=-10),border=ft.border.all(1,c_white))),
                                                     ft.Container(
                                                         
-                                                        width=560,
+                                                        width=860,
                                                         border = ft.border.all(1, c_white),
-                                                        padding=14
-                                            )]),
-                                            
-                                            
-                                                
+                                                        padding=14,
+                                                        height = 460,
+                                            )]),     
                                     ])),
-                                    width=560,
-                                    # height=80,
-                                    # bgcolor='red'
+                                    width=860,
                                 ),
                                 ft.Container(
-                                                ft.Container(
-                                                    ft.Row(controls=[
-                                                    ft.Container(ft.ElevatedButton(content = ft.Text('Назад',size=12,),data='Свободный фрейм',bgcolor=c_yelow,on_click=self.change_page,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,height=30),
-                                                    ft.Container(ft.ElevatedButton(content = ft.Text('Выбрать режим торговли',size=12,),data='Выбрать режим торговли',bgcolor=c_yelow,on_click=self.change_page,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,height=30),
-                                                ]),padding=ft.padding.only(left=130,top=10)
-                                                ),
-                                                width=560,
-                                                # height=100
-                                            )
+                                    ft.Container(
+                                        ft.Row(controls=[
+                                        ft.Container(ft.ElevatedButton(content = ft.Text('Назад',size=12,),data='Настройки робота',bgcolor=c_yelow,on_click=self.change_page,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,height=30),
+                                        ft.Container(ft.ElevatedButton(content = ft.Text('Выбрать режим торговли',size=12,),data='Выбрать режим торговли',bgcolor=c_yelow,on_click=self.change_page,color=c_blue,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0))),alignment=ft.alignment.center,height=30),
+                                    ]),padding=ft.padding.only(left=290,top=10)
+                                    ),
+                                    width=860,
+                                    # height=920
+                                )
                             ]),
                             alignment=ft.alignment.center),margin=ft.margin.only(top=2,left=-10,right=-10),padding=ft.padding.only(top=10)      
                     ),expand=2
