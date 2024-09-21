6fca24e2e360ffd3cd277ee18751eccbacbbaef0
MIN <maksimivankov26@yandex.ru>
2024-09-21 07:40:22 +0300

    общие и настройки стратегий ортрисованы

---
+++ src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/pages/trade_page/data_settings.py
@@ -0,0 +1,84 @@
+import flet as ft
+from variable import *
+from imports import *
+
+config = configparser.ConfigParser()  
+config.read(path_imports_config)
+
+# ----------------------------------------
+name_strat = {
+    'one':'Канал, тренд, локаль, объём',
+    'MA':'Скользящие средние'
+}
+strat_set_print = {
+    'one': ft.Container(ft.Column(controls=[
+        ft.Text(f'Верх канала - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_one_up_chanal')}',size=12),
+        ft.Text(f'Низ канала - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_one_down_chanal')}',size=12),
+        ft.Text(f'Угол тренда лонг - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_one_corner_long')}',size=12),
+        ft.Text(f'Угол тренда шорт - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_one_corner_short')}',size=12),
+        ])),
+    'MA': ft.Container(ft.Column(controls=[
+        ft.Text(f'Коэф. быстрой скольз. средней - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_ma_koef_bistro')}',size=12),
+        ft.Text(f'Коэф. медленной скольз. средней - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_ma_koef_medleno')}',size=12),
+        ft.Text(f'Кол-во совпадений в прошлом - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_ma_sovpad_last')}',size=12),
+        ft.Text(f'Прижатие к верху коридора - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_ma_up_chanal')}',size=12),
+        ft.Text(f'Прижатие к низу коридора - {config.get('param_trade_historical_trade_svobodniy_freym', 'strat_ma_down_chanal')}',size=12),
+        ])),
+}
+
+mas_strat_siroy = literal_eval(config.get('param_trade_historical_trade_svobodniy_freym', 'strategys'))
+strat_elements = []
+count_strat = 0
+for strat in mas_strat_siroy:
+    strat_elements.append(ft.Container(ft.Column(controls=[
+        ft.Text(f'Стратегия: {name_strat[strat]}'),
+        strat_set_print[strat]
+    ])))
+    count_strat+=1
+    if len(mas_strat_siroy) != count_strat:
+        strat_elements.append(ft.Container(width=350,height=1,bgcolor=c_white))
+    
+# ----------------------------------------
+
+print_our_settings = ft.Container(
+    ft.Row(controls=[
+        ft.Container(
+            ft.Column(controls=[
+                ft.Text(f'Имя робота для логов: {config.get('param_trade_historical_trade_svobodniy_freym', 'name_bot')}',size=12),
+                ft.Text(f'Режим монеты: {config.get('param_trade_historical_trade_svobodniy_freym', 'strategi_coin')}',size=12),
+                ft.Text(f'Сколько монет торговать: {config.get('param_trade_historical_trade_svobodniy_freym', 'how_mach_money')}',size=12),
+                ft.Text(f'Комиссия мейкер: {config.get('param_trade_historical_trade_svobodniy_freym', 'komission_mayker')}',size=12),
+                ft.Text(f'Комиссия тейкер: {config.get('param_trade_historical_trade_svobodniy_freym', 'komission_taker')}',size=12),
+                ft.Text(f'Таймфрейм: {config.get('param_trade_historical_trade_svobodniy_freym', 'work_tf')}',size=12),
+                ft.Text(f'Длительность торговли: {config.get('param_trade_historical_trade_svobodniy_freym', 'dlitelnost')}',size=12),
+            ],scroll=ft.ScrollMode.ALWAYS,),
+            width=230,height=140),
+        ft.Container(width=1,height=140,bgcolor=c_white),
+        ft.Container(
+            ft.Column(controls=[
+                ft.Text(f'Режим тейка/стопа: {config.get('param_trade_historical_trade_svobodniy_freym', 'regim_tp')}/{config.get('param_trade_historical_trade_svobodniy_freym', 'regim_sl')}',size=12),
+                ft.Text(f'Тейк профит: {config.get('param_trade_historical_trade_svobodniy_freym', 'tp')}',size=12),
+                ft.Text(f'Стоп лосс: {config.get('param_trade_historical_trade_svobodniy_freym', 'sl')}',size=12),
+                ft.Text(f'Режим объёмов: {config.get('param_trade_historical_trade_svobodniy_freym', 'regim_volume_min')}/{config.get('param_trade_historical_trade_svobodniy_freym', 'regim_volume_max')}',size=12),
+                ft.Text(f'Объём торгов мин: {config.get('param_trade_historical_trade_svobodniy_freym', 'volume_min')}',size=12),
+                ft.Text(f'Объём торгов макс: {config.get('param_trade_historical_trade_svobodniy_freym', 'volume_max')}',size=12),
+                ft.Text(f'Плечо: {config.get('param_trade_historical_trade_svobodniy_freym', 'leverage')}',size=12),
+            ],scroll=ft.ScrollMode.ALWAYS,),
+            width=230,height=140),
+    ]),padding=10
+)
+
+print_set_settings = ft.Container(
+    ft.Container(
+        ft.Column(controls=strat_elements,scroll=ft.ScrollMode.ALWAYS,),
+        width=350,height=140),padding=10
+)
+
+
+
+
+
+
+
+
+
--- src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/pages/trade_page/trade_page.py
+++ src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/pages/trade_page/trade_page.py
@@ -3,39 +3,14 @@ import flet as ft
 from variable import *
 from imports import *
 
+from src.trade_window.trade_windows_pages.components.content.hisorical_trade_page.pages.trade_page.data_settings import print_our_settings,print_set_settings
+
 class Trade_page(ft.UserControl):#1
     def __init__(self,change_page):
         super().__init__()
         self.change_page = change_page
-        config = configparser.ConfigParser()  
-        config.read(path_imports_config)
-        self.data_print_row = [
-            ['Имя робота для логов',config.get('param_trade_historical_trade_svobodniy_freym', 'name_bot'),'Режим тейка/стопа',f'{config.get('param_trade_historical_trade_svobodniy_freym', 'regim_tp')}/{config.get('param_trade_historical_trade_svobodniy_freym', 'regim_sl')}'],
-            ['Режим монеты',config.get('param_trade_historical_trade_svobodniy_freym', 'strategi_coin'),'Тейк профит',config.get('param_trade_historical_trade_svobodniy_freym', 'tp')],
-            ['Сколько монет торговать',config.get('param_trade_historical_trade_svobodniy_freym', 'how_mach_money'),'Стоп лосс',config.get('param_trade_historical_trade_svobodniy_freym', 'sl')],
-            ['Комиссия мейкер',config.get('param_trade_historical_trade_svobodniy_freym', 'komission_mayker'),'Режим объёмов',f'{config.get('param_trade_historical_trade_svobodniy_freym', 'regim_volume_min')}/{config.get('param_trade_historical_trade_svobodniy_freym', 'regim_volume_max')}'],
-            ['Комиссия тейкер',config.get('param_trade_historical_trade_svobodniy_freym', 'komission_taker'),'Объём торгов мин',config.get('param_trade_historical_trade_svobodniy_freym', 'volume_min')],
-            ['Таймфрейм',config.get('param_trade_historical_trade_svobodniy_freym', 'work_tf'),'Объём торгов макс',config.get('param_trade_historical_trade_svobodniy_freym', 'volume_max')],
-            ['Длительность торговли',config.get('param_trade_historical_trade_svobodniy_freym', 'dlitelnost'),'Плечо',config.get('param_trade_historical_trade_svobodniy_freym', 'leverage')],
-        ]
 
     def print_page(self):
-
-        datas_print = []
-        config_general_set = configparser.ConfigParser()  
-        config_general_set.read(path_ini_general_set)
-        for i in self.data_print_row:
-            datas_print.append(
-                ft.DataRow(cells=[
-                    ft.DataCell(ft.Text(i[0],text_align='center',color=c_white,size=12)),
-                    ft.DataCell(ft.Text(i[1],text_align='center',color=c_white,size=12)),
-                    ft.DataCell(ft.Text(i[2],text_align='center',color=c_white,size=12)),
-                    ft.DataCell(ft.Text(i[3],text_align='center',color=c_white,size=12)),
-                    
-                    
-                ]
-                )
-            )  
 
         self.trade_page = ft.Container(
             ft.Container(
@@ -51,25 +26,21 @@ class Trade_page(ft.UserControl):#1
                                                 controls=[
                                                     ft.Container(
                                                         ft.Container(ft.Text('Общие настройки робота',color=c_blue,),bgcolor=c_yelow,padding=5,margin=ft.margin.only(bottom=-10),border=ft.border.all(1,c_white))),
-                                                    ft.Container(
                                                         ft.Container(
-                                                            ft.Column(
-                                                                controls=[
-                                                            
-                                                            ],
-                                                            scroll=ft.ScrollMode.ALWAYS,),
+                                                            ft.Container(
+                                                                print_our_settings,
+                                                                width=500,
+                                                                height=148,
+                                                                border = ft.border.all(1, c_white),
+                                                                bgcolor=c_blue,
+                                                            ),
                                                             width=500,
-                                                            height=148,
-                                                            border = ft.border.all(1, c_white),
-                                                            bgcolor=c_blue,
-                                                        ),
-                                                        width=500,
-                                                        # border = ft.border.all(1, c_white),
-                                                        # padding=14,
-                                                        height = 148,
-                                                        padding=ft.padding.only(left=-1,top=-1,bottom=-1)
+                                                            # border = ft.border.all(1, c_white),
+                                                            # padding=14,
+                                                            height = 148,
+                                                            padding=ft.padding.only(left=-1,top=-1,bottom=-1)
                                                         
-                                            )]),     
+                                                        )]),     
                                     ])),
                                     width=500,
                                     # bgcolor='red'
@@ -83,11 +54,7 @@ class Trade_page(ft.UserControl):#1
                                                             ft.Container(ft.Text('Настройки стратегий',color=c_blue,),bgcolor=c_yelow,padding=5,margin=ft.margin.only(bottom=-10),border=ft.border.all(1,c_white))),
                                                         ft.Container(
                                                             ft.Container(
-                                                                ft.Column(
-                                                                    controls=[
-                                                                
-                                                                ],
-                                                                scroll=ft.ScrollMode.ALWAYS,),
+                                                                print_set_settings,
                                                                 width=350,
                                                                 height=148,
                                                                 border = ft.border.all(1, c_white),
