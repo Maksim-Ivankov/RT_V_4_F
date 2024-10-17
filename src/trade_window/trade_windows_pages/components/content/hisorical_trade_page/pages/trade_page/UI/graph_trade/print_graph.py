474701ddd0bbd12627b6ce7d5e96a47fbbf3ae20
MIN <maksimivankov26@yandex.ru>
2024-10-17 18:53:52 +0300

    графики полностью готовы

--- src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/pages/trade_page/UI/graph_trade/print_graph.py
+++ src/trade_window/trade_windows_pages/components/content/hisorical_trade_page/pages/trade_page/UI/graph_trade/print_graph.py
@@ -43,7 +43,7 @@ class Print_graph(ft.UserControl):
         self.print_canvas_arr_min = [] # сюда сохраняем все что нжно отрисовать в канвасе мини
 
     def pan_start(self,e: ft.DragStartEvent):
-        print(e.local_y)
+        # print(e.local_y)
         # self.canvas.scan_dragto(event.x, event.y, gain=1)
         # self.canvas_price.scan_dragto(0, event.y, gain=1)
         # self.canvas_date.scan_dragto(event.x, 0, gain=1)
@@ -61,15 +61,23 @@ class Print_graph(ft.UserControl):
         # self.graph_trade.scale
         # print(self.graph_trade.shapes)
         # pass
-        self.line_price.y1 = e.local_y
-        self.line_price.y2 = e.local_y
-        self.line_price_rect.elements[0].elements[0].y = e.local_y-10
+        self.line_price_horizont.y1 = e.local_y
+        self.line_price_horizont.y2 = e.local_y
+        self.line_price_rect_horizont.elements[0].elements[0].y = e.local_y-10
+        # self.line_price_rect_vertical.elements[0].elements[0].x = e.local_x-20
         self.price_value_line.y = e.local_y-8
+        # self.data_value_text.x = e.local_x-8
         if self.form_graph == 'max':
-            text_price = (((self.set['height_graph'] - e.local_y)*self.OldRange)/self.set['height_graph'])+self.price_min
+            text_price = (((self.set['height_graph']*0.7 - e.local_y)*self.OldRange)/(self.set['height_graph']*0.7))+self.price_min
+            # text_data = int(e.local_x*self.OldRange1)/(self.NewRange1*0.95)
         if self.form_graph == 'min':
-            text_price = (((self.set['height_graph'] - e.local_y)*self.OldRange_min)/self.set['height_graph'])+self.price_min_min
+            text_price = (((self.set['height_graph']*0.7 - e.local_y)*self.OldRange_min)/(self.set['height_graph']*0.7))+self.price_min_min
+            # text_data = int(e.local_x*self.OldRange1_min)/(self.NewRange1_min*0.95)
         self.price_value_line.text = round(text_price,5)
+        # self.data_value_text.text = round(text_data,0)
+        #-----------------
+        self.line_price_vertical.x1 = e.local_x
+        self.line_price_vertical.x2 = e.local_x
         # self.price_value_line.text = text_price
         self.update()
         # pass
@@ -80,8 +88,8 @@ class Print_graph(ft.UserControl):
 
         # работа с макс графиком
         # определяем границы для масштабирования графика цены
-        self.price_max = (self.df.loc[self.df['close'] == self.df['close'].max()].iloc[0]['close'])
-        self.price_min = (self.df.loc[self.df['close'] == self.df['close'].min()].iloc[0]['close'])
+        self.price_max = (self.df.loc[self.df['high'] == self.df['high'].max()].iloc[0]['high'])
+        self.price_min = (self.df.loc[self.df['low'] == self.df['low'].min()].iloc[0]['low'])
         self.OldRange = (self.price_max - self.price_min) 
         self.OldRange1 = len(self.df)
         self.NewRange1 = self.set['width_graph']/(len(self.df)/self.OldRange1)  # МАГИЧЕСКОЕ ЧИСЛО - 144 ЧТО ЭТО?
@@ -92,8 +100,8 @@ class Print_graph(ft.UserControl):
         NewRange_volume = 110                                           # МАГИЧЕСКОЕ ЧИСЛО - 110 ЧТО ЭТО?
 
         # работа с мини графиком
-        self.price_max_min = (self.df_min.loc[self.df_min['close'] == self.df_min['close'].max()].iloc[0]['close'])
-        self.price_min_min = (self.df_min.loc[self.df_min['close'] == self.df_min['close'].min()].iloc[0]['close'])
+        self.price_max_min = (self.df_min.loc[self.df_min['high'] == self.df_min['high'].max()].iloc[0]['high'])
+        self.price_min_min = (self.df_min.loc[self.df_min['low'] == self.df_min['low'].min()].iloc[0]['low'])
         self.OldRange_min = (self.price_max_min - self.price_min_min) 
         self.OldRange1_min = len(self.df_min)
         self.NewRange1_min = self.set['width_graph']/(len(self.df_min)/self.OldRange1_min)  # МАГИЧЕСКОЕ ЧИСЛО - 144 ЧТО ЭТО?
@@ -103,8 +111,8 @@ class Print_graph(ft.UserControl):
         NewRange_volume_min = 110     
 
         # рисуем стоп и тейк
-        y_stop = self.set['width_graph']*0.7 - (((float(self.set['SL']) - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
-        y_take = self.set['width_graph']*0.7 - (((float(self.set['TP']) - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
+        y_stop = self.set['height_graph']*0.7 - (((float(self.set['SL']) - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
+        y_take = self.set['height_graph']*0.7 - (((float(self.set['TP']) - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
         # print(f'y стоп = {y_stop}')
         # print(f'SL = {float(self.set['SL'])}')
         # print(f'self.price_min_min = {self.price_min_min}')
@@ -125,10 +133,10 @@ class Print_graph(ft.UserControl):
         for index, row in self.df.iterrows():
             x0 = ((index * self.NewRange1) / self.OldRange1)*0.95
             # self.mass_date_interval_graph[(x0,x0)] = datetime.fromtimestamp(int(row['open_time']/1000)).strftime('%d.%m.%Y %H:%M')
-            y0 = self.set['width_graph']*0.7 - (((row['open'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
-            y1 = self.set['width_graph']*0.7 - (((row['close'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
-            high = self.set['width_graph']*0.7 - (((row['high'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
-            low = self.set['width_graph']*0.7 - (((row['low'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
+            y0 = self.set['height_graph']*0.7 - (((row['open'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
+            y1 = self.set['height_graph']*0.7 - (((row['close'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
+            high = self.set['height_graph']*0.7 - (((row['high'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
+            low = self.set['height_graph']*0.7 - (((row['low'] - self.price_min) * self.set['height_graph']*0.7) / self.OldRange)
             if index == int(self.set['index_entry']):
                 self.print_trade_point('entry',x0,y1,'max')
             if index == int(self.set['index_exit']):
@@ -146,10 +154,10 @@ class Print_graph(ft.UserControl):
             # x0 = ((index * self.NewRange1_min) / self.OldRange1_min)*0.95
             # print(f'{count_see} = {x0}')
             # self.mass_date_interval_graph[(x0,x0)] = datetime.fromtimestamp(int(row['open_time']/1000)).strftime('%d.%m.%Y %H:%M')
-            y0 = self.set['width_graph']*0.7 - (((row['open'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
-            y1 = self.set['width_graph']*0.7 - (((row['close'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
-            high = self.set['width_graph']*0.7 - (((row['high'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
-            low = self.set['width_graph']*0.7 - (((row['low'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
+            y0 = self.set['height_graph']*0.7 - (((row['open'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
+            y1 = self.set['height_graph']*0.7 - (((row['close'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
+            high = self.set['height_graph']*0.7 - (((row['high'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
+            low = self.set['height_graph']*0.7 - (((row['low'] - self.price_min_min) * self.set['height_graph']*0.7) / self.OldRange_min)
             if index == int(self.index_open):
                 self.print_trade_point('entry',x0,y1,'min')
             if index == int(self.index_close):
@@ -195,16 +203,6 @@ class Print_graph(ft.UserControl):
             
     def print_trade_point(self,point,x0,y1,form_graph):
         # print('Рисуем треугольник')
-        # if point == 'entry':
-        #     if self.set['trend'] == 'long':
-        #         self.print_canvas_arr.append(cv.Path([cv.Path.SubPath([cv.Path.MoveTo(x0+10,y1+5),cv.Path.LineTo(x0+20,y1+20),cv.Path.LineTo(x0+0,y1+20),cv.Path.LineTo(x0+10,y1+5),],0, 0)],paint=self.fill_green))
-        #     elif self.set['trend'] == 'short':
-        #         self.print_canvas_arr.append(cv.Path([cv.Path.SubPath([cv.Path.MoveTo(x0,y1),cv.Path.LineTo(x0-10,y1-10),cv.Path.LineTo(x0+10,y1-10),cv.Path.LineTo(x0,y1),],0, 0)],paint=self.fill_red))
-        # if point == 'exit':
-        #     if self.set['trend'] == 'long':
-        #         self.print_canvas_arr.append(cv.Path([cv.Path.SubPath([cv.Path.MoveTo(x0,y1),cv.Path.LineTo(x0+10,y1+10),cv.Path.LineTo(x0-10,y1+10),cv.Path.LineTo(x0,y1),],0, 0)],paint=self.fill_green))
-        #     elif self.set['trend'] == 'short':
-        #         self.print_canvas_arr.append(cv.Path([cv.Path.SubPath([cv.Path.MoveTo(x0,y1),cv.Path.LineTo(x0-10,y1-10),cv.Path.LineTo(x0+10,y1-10),cv.Path.LineTo(x0,y1),],0, 0)],paint=self.fill_red))
         if form_graph=='max':
             if self.set['trend'] == 'long':
                 if point == 'entry': self.print_canvas_arr.append(cv.Path([cv.Path.SubPath([cv.Path.MoveTo(x0,y1),cv.Path.LineTo(x0+10,y1+10),cv.Path.LineTo(x0-10,y1+10),cv.Path.LineTo(x0,y1),],0, 0)],paint=self.fill_green))
@@ -234,12 +232,15 @@ class Print_graph(ft.UserControl):
         self.fill_blue = ft.Paint(style=ft.PaintingStyle.FILL,color=c_blue)
         line_price_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE,color=c_gray_binance)
         # сноска с ценой
-        # линия прайса
-        self.line_price = cv.Line(0,0,self.set['width_graph']-20,2, line_price_paint)
-        # прямоугольник прайса
-        self.line_price_rect = cv.Path([cv.Path.SubPath([cv.Path.Rect(0, 0, 50, 20)],0, 0)],paint=self.fill_rect_price)
-        # прайс
+        # линия прайса горизонт и вертикаль
+        self.line_price_horizont = cv.Line(0,0,self.set['width_graph']-20,2, line_price_paint)
+        self.line_price_vertical = cv.Line(0,0,2,self.set['height_graph']-20, line_price_paint)
+        # прямоугольник прайса горизонт и вертикаль
+        self.line_price_rect_horizont = cv.Path([cv.Path.SubPath([cv.Path.Rect(0, 0, 50, 20)],0, 0)],paint=self.fill_rect_price)
+        # self.line_price_rect_vertical = cv.Path([cv.Path.SubPath([cv.Path.Rect(0, 0, 40, 20)],0, self.set['height_graph']-30)],paint=self.fill_rect_price)
+        # прайс горизонт и вертикаль
         self.price_value_line = cv.Text(5,5,'111',ft.TextStyle(size=12,color='#b6b8b1'))
+        # self.data_value_text = cv.Text(5,self.set['height_graph']-25,'111',ft.TextStyle(size=12,color='#b6b8b1'))
 
         self.paint_bar()
 
@@ -250,9 +251,12 @@ class Print_graph(ft.UserControl):
         # self.print_canvas_arr.append(self.line_price)
         # отрисовка полного графика
         if self.form_graph == 'max':
-            self.print_canvas_arr.append(self.line_price)
-            self.print_canvas_arr.append(self.line_price_rect)
+            self.print_canvas_arr.append(self.line_price_horizont)
+            self.print_canvas_arr.append(self.line_price_rect_horizont)
             self.print_canvas_arr.append(self.price_value_line)
+            self.print_canvas_arr.append(self.line_price_vertical)
+            # self.print_canvas_arr.append(self.line_price_rect_vertical)
+            # self.print_canvas_arr.append(self.data_value_text)
             # self.NewRange_price = 
             # self.print_canvas_arr.append(self.line_price)
             # print('Внутри print_graph - рисуем большой')
@@ -264,9 +268,12 @@ class Print_graph(ft.UserControl):
             ))
         # отрисовка урезанного графика, только со сделкой
         if self.form_graph == 'min':
-            self.print_canvas_arr_min.append(self.line_price)
-            self.print_canvas_arr_min.append(self.line_price_rect)
+            self.print_canvas_arr_min.append(self.line_price_horizont)
+            self.print_canvas_arr_min.append(self.line_price_rect_horizont)
             self.print_canvas_arr_min.append(self.price_value_line)
+            self.print_canvas_arr_min.append(self.line_price_vertical)
+            # self.print_canvas_arr_min.append(self.line_price_rect_vertical)
+            # self.print_canvas_arr_min.append(self.data_value_text)
             # print('Внутри print_graph - рисуем малый')
             # print(self.print_canvas_arr_min)
             # self.print_canvas_arr_test.append(cv.Path([cv.Path.SubPath([cv.Path.Rect(0, 0,100, 100)],0, 0)],paint=self.fill_green))
